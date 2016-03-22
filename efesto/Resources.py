65c6ea369954e54c8743357cf60ceadd7090c2d5
vesuvium <jacopocascioli@gmail.com>
2016-03-22 16:36:32 +0100

    Added Resources module

---
+++ efesto/Resources.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    Resources
+"""
+import falcon
+
+
+from .Models import *
+
+
+def make_resource(name, model):
+    """
+    Generates a resource class.
+    """
+    pass
