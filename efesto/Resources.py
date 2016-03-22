c6ce656bc2ee3f100e12d38811c2698e32081f97
vesuvium <jacopocascioli@gmail.com>
2016-03-22 17:15:37 +0100

    Updated make_resource with missing methods

--- efesto/Resources.py
+++ efesto/Resources.py
@@ -12,4 +12,22 @@ def make_resource(model):
     """
     Generates a resource class.
     """
-    pass
+    def on_get(self, request, response):
+        pass
+
+    def on_post():
+        pass
+
+    def on_delete():
+        pass
+
+    def on_patch():
+        pass
+
+    attributes = {
+        'on_get': on_get,
+        'on_post': on_post,
+        'on_patch': on_patch,
+        'on_delete': on_delete
+    }
+    return type('myresource', (object, ), attributes)
