3fb0a6a4579c1af55ba1ee6b993ad42badc4a310
vesuvium <jacopocascioli@gmail.com>
2016-03-22 17:06:33 +0100

    Parametrized test_make_resource

--- tests/Resources.py
+++ tests/Resources.py
@@ -6,4 +6,14 @@ sys.path.insert(0, "")
 import pytest
 
 
+from efesto.Models import *
 from efesto.Resources import *
+
+
+@pytest.mark.parametrize('model', [Users])
+@pytest.mark.parametrize('method',
+    ['on_get', 'on_post', 'on_patch', 'on_delete']
+)
+def test_make_resource(model, method):
+    resource = make_resource(model)
+    assert hasattr(resource, method) == True
