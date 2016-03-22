c0d9e719af396e2b193c5543a330e18cf15dc9f6
vesuvium <jacopocascioli@gmail.com>
2016-03-22 15:49:30 +0100

    Added Models.py

---
+++ efesto/Models.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+    The models used by Efesto.
+"""
+from peewee import *
+
+
+db = PostgresqlDatabase(
+    'test',
+    user='postgres',
+    password='postgres',
+    host='localhost'
+)
+
+
+class Base(Model):
+    """A base model that will use our Postgresql database"""
+    class Meta:
+        database = db
+
+
+class Users(Base):
+    id = IntegerField(primary_key=True)
+    name = CharField()
+    email = CharField()
+    password = CharField()
+    rank = IntegerField()
+    last_login = DateTimeField()
