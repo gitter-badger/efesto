302aa18ca1b39bb212c4503aadc686b45c5462d6
vesuvium <jacopocascioli@gmail.com>
2016-03-23 15:46:47 +0100

    Added Auth module

---
+++ efesto/Auth.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+    The Efesto authentication module.
+
+    Efesto uses Hawk (https://github.com/hueniverse/hawk) for authentication.
+"""
+from mohawk import Receiver
---
+++ tests/Auth.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+    The Auth test case.
+"""
+import sys
+sys.path.insert(0, "")
+import pytest
