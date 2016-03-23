35420371497fe5d42d25590c7f8c4375d7dbea49
vesuvium <jacopocascioli@gmail.com>
2016-03-23 11:56:59 +0100

    Overhauled models tests

--- tests/Models.py
+++ tests/Models.py
@@ -7,42 +7,88 @@
 import sys
 sys.path.insert(0, "")
 import pytest
+from peewee import IntegerField, CharField, DateTimeField, BooleanField
 
 
 from efesto.Models import Users, Types, Fields, AccessRules
 
 
-@pytest.mark.parametrize('column',
-    ['id', 'name', 'email', 'password', 'rank', 'last_login']
+@pytest.mark.parametrize('column_dict',
+    [
+        { 'column': 'id', 'field': IntegerField },
+        { 'column': 'name', 'field': CharField },
+        { 'column': 'email', 'field': CharField },
+        { 'column': 'password', 'field': CharField },
+        { 'column': 'rank', 'field': IntegerField },
+        { 'column': 'last_login', 'field': DateTimeField }
+    ]
 )
-def test_users_model(column):
+def test_users_model(column_dict):
     """
     Tests the Users model.
     """
-    assert column in Users.__dict__
+    column = column_dict['column']
+    field = column_dict['field']
+    field_object = getattr(Users, column)
+    assert isinstance(field_object, field)
 
 
-@pytest.mark.parametrize('column', ['id','name', 'enabled'])
-def test_types_model(column):
+@pytest.mark.parametrize('column_dict',
+    [
+        { 'column': 'id', 'field': IntegerField },
+        { 'column': 'name', 'field': CharField },
+        { 'column': 'enabled', 'field': BooleanField }
+    ]
+)
+def test_types_model(column_dict):
     """
     Tests the Types model.
     """
-    assert column in Types.__dict__
+    column = column_dict['column']
+    field = column_dict['field']
+    field_object = getattr(Types, column)
+    assert isinstance(field_object, field)
 
 
-@pytest.mark.parametrize('column', ['id', 'name', 'type', 'foreign', 'unique', 'description', 'label'])
-def test_fields_model(column):
+@pytest.mark.parametrize('column_dict',
+    [
+        { 'column': 'id', 'field': IntegerField },
+        { 'column': 'name', 'field': CharField },
+        { 'column': 'type', 'field': IntegerField },
+        { 'column': 'foreign', 'field': CharField },
+        { 'column': 'unique', 'field': BooleanField },
+        { 'column': 'description', 'field': CharField },
+        { 'column': 'label', 'field': CharField }
+    ]
+)
+def test_fields_model(column_dict):
     """
     Tests the Fields model.
     """
-    assert column in Fields.__dict__
+    column = column_dict['column']
+    field = column_dict['field']
+    field_object = getattr(Fields, column)
+    assert isinstance(field_object, field)
 
 
-@pytest.mark.parametrize('column',
-    ['id','user', 'rank', 'item', 'type', 'level', 'read', 'edit', 'delete']
+@pytest.mark.parametrize('column_dict',
+    [
+        { 'column': 'id', 'field': IntegerField },
+        { 'column': 'user', 'field': IntegerField },
+        { 'column': 'rank', 'field': IntegerField },
+        { 'column': 'item', 'field': IntegerField },
+        { 'column': 'type', 'field': IntegerField },
+        { 'column': 'level', 'field': IntegerField },
+        { 'column': 'read', 'field': IntegerField },
+        { 'column': 'edit', 'field': IntegerField },
+        { 'column': 'delete', 'field': IntegerField }
+    ]
 )
-def test_access_rules_model(column):
+def test_access_rules_model(column_dict):
     """
     Tests the AccessRules model.
     """
-    assert column in AccessRules.__dict__
+    column = column_dict['column']
+    field = column_dict['field']
+    field_object = getattr(AccessRules, column)
+    assert isinstance(field_object, field)
