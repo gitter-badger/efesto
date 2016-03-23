972fc54d42fa33ae53270743533d4e7818c43c3b
vesuvium <jacopocascioli@gmail.com>
2016-03-23 17:41:15 +0100

    Added docstrings

--- efesto/Models.py
+++ efesto/Models.py
@@ -20,6 +20,9 @@ class Base(Model):
 
 
 class Users(Base):
+    """
+    Users.
+    """
     id = IntegerField(primary_key=True)
     name = CharField()
     email = CharField()
@@ -29,12 +32,20 @@ class Users(Base):
 
 
 class Types(Base):
+    """
+    The Types specify the custom types that should be generated. Only enabled
+    types will be generated.
+    """
     id = IntegerField(primary_key=True)
     name = CharField()
     enabled = BooleanField()
 
 
 class Fields(Base):
+    """
+    The fields are used to generate the columns for custom models/tables for
+    the types specified in Types.
+    """
     id = IntegerField(primary_key=True)
     name = CharField()
     type = ForeignKeyField(Types)
@@ -45,6 +56,10 @@ class Fields(Base):
 
 
 class AccessRules(Base):
+    """
+    AccessRules define the permissions that an users or a group of users have
+    on a single item or on a group of items.
+    """
     id = IntegerField(primary_key=True)
     user = ForeignKeyField(Users, null=True)
     rank = IntegerField(null=True)
