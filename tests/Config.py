c1d54997ec0119222a581b0f1895728e7f5d198e
vesuvium <jacopocascioli@gmail.com>
2016-03-24 15:14:32 +0100

    Added test for find_path

--- tests/Config.py
+++ tests/Config.py
@@ -6,6 +6,7 @@
 """
 import sys
 sys.path.insert(0, "")
+import os
 import pytest
 
 
@@ -15,6 +16,15 @@ from efesto.Config import Config
 @pytest.fixture
 def config():
     return Config()
+
+
+def test_find_path(config):
+    path = 'test.cfg'
+    f = open(path, 'w')
+    f.close()
+    fullpath = config.find_path(path)
+    os.remove(path)
+    assert fullpath == os.path.abspath(path)
 
 
 def test_find_path_exception(config):
