4a67c94820451dca36f75498d880184025a79b44
vesuvium <jacopocascioli@gmail.com>
2016-03-24 17:38:12 +0100

    Updated Auth module

--- efesto/Auth.py
+++ efesto/Auth.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 """
     The Efesto authentication module.
-
-    Efesto uses Hawk (https://github.com/hueniverse/hawk) for authentication.
 """
-from mohawk import Receiver
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+
+from .Models import Users
+from .Config import Config
+
+
+config = Config()
+def generate_token(expiration=600, **kwargs):
+    s = Serializer(config.parser.get('main', 'secret'), expires_in=expiration)
+    return s.dumps(kwargs)
