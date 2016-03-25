977e1e70ca30aa90dce0f708a1f1943fa642e869
vesuvium <jacopocascioli@gmail.com>
2016-03-25 16:55:36 +0100

    Added read_token

--- efesto/Auth.py
+++ efesto/Auth.py
@@ -13,3 +13,8 @@ config = Config()
 def generate_token(expiration=600, **kwargs):
     s = Serializer(config.parser.get('main', 'secret'), expires_in=expiration)
     return s.dumps(kwargs)
+
+
+def read_token(token):
+    s = Serializer(config.parser.get('main', 'secret'))
+    return s.loads(token)
