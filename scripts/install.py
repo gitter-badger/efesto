7d61a76450fc4e7b4c5506abb2c3bc796e3349c6
vesuvium <jacopocascioli@gmail.com>
2016-03-30 10:47:22 +0200

    install script uses config from Base

--- scripts/install.py
+++ scripts/install.py
@@ -3,12 +3,10 @@ import sys
 sys.path.insert(0, "")
 
 
-from efesto.Config import Config
-from efesto.Base import db
+from efesto.Base import db, config
 from efesto.Models import Users, Types, Fields, AccessRules
 
 
-config = Config()
 if config.parser.getboolean('main', 'installed') != True:
     db.create_tables([Users, Types, Fields, AccessRules])
     config.parser.set('main', 'installed', 'True')
