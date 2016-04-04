7ac925e7e4144978fef838855486776dfca41071
vesuvium <jacopocascioli@gmail.com>
2016-04-04 16:00:49 +0200

    Updated installation script

--- scripts/install.py
+++ scripts/install.py
@@ -4,10 +4,10 @@ sys.path.insert(0, "")
 
 
 from efesto.Base import db, config
-from efesto.Models import Users, Types, Fields, AccessRules
+from efesto.Models import Users, Types, Fields, AccessRules, EternalTokens
 
 
 if config.parser.getboolean('main', 'installed') != True:
-    db.create_tables([Users, Types, Fields, AccessRules])
+    db.create_tables([Users, Types, Fields, AccessRules, EternalTokens])
     config.parser.set('main', 'installed', 'True')
     #config.parser.write(config.path)
