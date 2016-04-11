35a3446c678be67400a76edd3255b90615b1dc85
vesuvium <jacopocascioli@gmail.com>
2016-04-11 12:29:11 +0200

    Implemented test_rank_override_stack_by_model

--- tests/Permissions.py
+++ tests/Permissions.py
@@ -259,8 +259,25 @@ def test_rank_override_check_model():
     raise NotImplementedError("Not implemented!")
 
 
-def test_rank_override_stack_by_model():
-    raise NotImplementedError("Not implemented!")
+def test_rank_override_stack_by_model(dummy_user, action, item):
+    """
+    Tests permissions when there are more rules for the same rank.
+    """
+    # set up
+    model_name = getattr(item._meta, 'db_table')
+    rule_dict = {'rank': dummy_user.rank, 'level':2, 'model':model_name}
+    rule_dict[action] = 1
+    rule = AccessRules(**rule_dict)
+    rule.save()
+    new_dict = {'rank': dummy_user.rank, 'level':3, 'model':model_name}
+    new_dict[action] = 0
+    new_rule = AccessRules(**new_dict)
+    new_rule.save()
+    # test
+    assert dummy_user.can(action, item) == False
+    # tear down
+    new_rule.delete_instance()
+    rule.delete_instance()
 
 
 def test_rank_override_by_item():
