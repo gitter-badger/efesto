ef6085c05846372956396e35597882f55bc2f45d
vesuvium <jacopocascioli@gmail.com>
2016-04-11 12:33:06 +0200

    Implemented test_rank_override_check_model

--- tests/Permissions.py
+++ tests/Permissions.py
@@ -255,8 +255,46 @@ def test_rank_override_by_model(dummy_user, action, item):
     rule.delete_instance()
 
 
-def test_rank_override_check_model():
-    raise NotImplementedError("Not implemented!")
+@pytest.mark.parametrize('args', [
+    {
+        'model':Users, 'args':{'name':'u', 'email':'mail', 'password':'p', 'rank':1},
+        'model2': Types, 'args2':{'name':'mytype', 'enabled':0}
+    },
+    {
+        'model':Types, 'args': {'name':'mytype', 'enabled':0},
+        'model2':AccessRules, 'args2':{'level': 1}
+    },
+    {
+        'model':AccessRules, 'args': {'level': 1}, 'model2':Users,
+        'args2': {'name':'u', 'email':'mail', 'password':'p', 'rank':1}
+    }
+])
+def test_rank_override_check_model(dummy_user, action, args):
+    """
+    Verifies that permissions rules set for a rank affect only the specified
+    model.
+    """
+    # set up
+    model = args['model']
+    model_name = getattr(model._meta, 'db_table')
+    rule_dict = {'rank':dummy_user.rank, 'level':2, 'model':model_name}
+    rule_dict[action] = 1
+    rule = AccessRules(**rule_dict)
+    rule.save()
+    item = args['model2'](**args['args2'])
+    item.save()
+    actions = ['read', 'edit', 'eliminate']
+    actions.remove(action)
+    # test
+    new_item = model(**args['args'])
+    new_item.save()
+    assert dummy_user.can(action, new_item) == True
+    for i in actions:
+        assert dummy_user.can(i, item) == False
+    # tear down
+    item.delete_instance()
+    new_item.delete_instance()
+    rule.delete_instance()
 
 
 def test_rank_override_stack_by_model(dummy_user, action, item):
