20bdf0232c37c5bca83c32f3a717a114e4778673
vesuvium <jacopocascioli@gmail.com>
2016-04-14 17:15:25 +0200

    dummy_admin fixture calls delete_instance recursively

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,7 +24,7 @@ def dummy_admin(request):
     dummy.save()
 
     def teardown():
-        dummy.delete_instance()
+        dummy.delete_instance(recursive=True)
     request.addfinalizer(teardown)
     return dummy
 
