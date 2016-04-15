aa56890a67b08ad0b7d0ed05fe6c5bbd801a5cda
vesuvium <jacopocascioli@gmail.com>
2016-04-15 15:33:04 +0200

    Updated __init__

--- efesto/__init__.py
+++ efesto/__init__.py
@@ -5,5 +5,23 @@
 import falcon
 
 
-from .Base import db, config
-from .Resources import *
+from .Resources import make_collection, make_resource, TokensResource
+from .Models import (Users, Types, Fields, AccessRules, EternalTokens)
+
+
+app = falcon.API()
+for i in [['/users', Users], ['/types', Types], ['/fields', Fields],
+        ['/rules', AccessRules], ['/tokens', EternalTokens]]:
+    collection = make_collection(i[1])()
+    resource = make_resource(i[1])()
+    app.add_route(i[0], collection)
+    app.add_route("%s/{id}" % i[0], resource)
+
+app.add_route("/auth", TokensResource())
+
+
+def error_serializer(req, exception):
+    preferred = 'application/json'
+    representation = exception.to_json()
+    return (preferred, representation)
+app.set_error_serializer(error_serializer)
