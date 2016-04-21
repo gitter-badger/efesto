6e3c87374896332227075f98e77738e9ed2fffe9
vesuvium <jacopocascioli@gmail.com>
2016-04-21 12:56:30 +0200

    Overhauled install script

--- scripts/install.py
+++ scripts/install.py
@@ -10,25 +10,73 @@ sys.path.insert(0, "")
 import os
 from binascii import hexlify
 import getpass
+from colorama import Fore, Style
+from peewee import ProgrammingError, OperationalError
 
 
 from efesto.Base import db, config
 from efesto.Models import Users, Types, Fields, AccessRules, EternalTokens
 
 
-print('This script will install Efesto for you. ')
-admin_name = input('Administrator name: ')
-admin_email = input('Administrator email: ')
-admin_password = getpass.getpass('Administrator password: ')
-secret_key = str(hexlify(os.urandom(24)), encoding="utf8")
+def message(message, colour):
+    """
+    Prints a coloured message using colorama.
+    """
+    text_colours = {'green': Fore.GREEN,  'red': Fore.RED, 'blue': Fore.BLUE}
+    print(text_colours[colour] + message + Style.RESET_ALL)
 
 
-if config.parser.getboolean('main', 'installed') != True:
+def create_tables():
+    """
+    Creates the tables.
+    """
+    try:
+        db.create_tables([Users, Types, Fields, AccessRules, EternalTokens])
+        message('Tables created!', 'green')
+    except OperationalError:
+        message('An error occured during tables creation. Please check your database credentials.', 'red')
+        exit()
+    except ProgrammingError:
+        message('An error occurred during tables creation. Please check your database.', 'red')
+        exit()
+    except:
+        message('An unknown error occurred during tables creation', 'red')
+        exit()
+
+
+def create_config():
+    secret_key = str(hexlify(os.urandom(24)), encoding="utf8")
     config.parser.set('main', 'installed', 'True')
     config.parser.set('security', 'secret', secret_key)
     with open(config.path, 'w') as configfile:
         config.parser.write(configfile)
+        message('Configuration successfully updated!', 'green')
 
-    db.create_tables([Users, Types, Fields, AccessRules, EternalTokens])
+
+def create_admin():
+    """
+    Creates the default administrator.
+    """
+    print('Efesto will now create the default administrator')
+    admin_name = input('Administrator name: ')
+    admin_email = input('Administrator email: ')
+    admin_password = getpass.getpass('Administrator password: ')
     admin = Users(name=admin_name, email=admin_email, rank=10, password=admin_password)
     admin.save()
+    print(Fore.GREEN + 'Admin created!')
+
+
+def install():
+    installed = config.parser.getboolean('main', 'installed')
+    if installed != True:
+        message('Installing...', 'blue')
+        create_tables()
+        create_config()
+        create_admin()
+        message('Installation successful.', 'green')
+    else:
+        message('It seems Efesto is already installed!', 'red')
+
+
+if __name__ == '__main__':
+    install()
