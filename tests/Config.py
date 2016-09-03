ea0da8c876358b30928204ec4b1114106e6bc8a4
Jacopo <jacopocascioli@gmail.com>
2016-09-04 01:06:11 +0200

    tests(config): test_find_path uses config_file fixture

--- tests/Config.py
+++ tests/Config.py
@@ -20,13 +20,9 @@ def config():
     return Config()
 
 
-def test_find_path(config):
-    path = 'test.cfg'
-    f = open(path, 'w')
-    f.close()
-    fullpath = config.find_path(path)
-    os.remove(path)
-    assert fullpath == os.path.abspath(path)
+def test_find_path(config, config_file):
+    fullpath = config.find_path(config_file)
+    assert fullpath == os.path.abspath(config_file)
 
 
 def test_find_path_parent_folder(config):
