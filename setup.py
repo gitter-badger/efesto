f5f3289e44554c211b10768441ce759725fd672d
Jacopo Cascioli <jacopocascioli@gmail.com>
2016-10-27 09:54:59 +0200

    config(app): version bump to 0.7.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author='Jacopo Cascioli',
     author_email='jacopocascioli@gmail.com',
     license='GPL3',
-    version='0.7.2',
+    version='0.7.3',
     packages=find_packages(),
     tests_require=[
         'pytest',
