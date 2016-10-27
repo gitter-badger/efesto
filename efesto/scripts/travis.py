c4aa775fc71d5b7c21434045fcd937dd2171cfa4
Jacopo Cascioli <jacopocascioli@gmail.com>
2016-10-27 10:09:11 +0200

    fix(travis): use strings

--- efesto/scripts/travis.py
+++ efesto/scripts/travis.py
@@ -16,10 +16,10 @@ def create_travis_config():
     config.parser.set('db', 'name', 'test')
     config.parser.set('db', 'user', 'postgres')
     config.parser.set('db', 'password', '')
-    config.parser.set('cors', 'all_origins', True)
-    config.parser.set('cors', 'all_methods', True)
-    config.parser.set('cors', 'all_headers', True)
-    config.parser.set('cors', 'all_credentials', True)
+    config.parser.set('cors', 'all_origins', 'True')
+    config.parser.set('cors', 'all_methods', 'True')
+    config.parser.set('cors', 'all_headers', 'True')
+    config.parser.set('cors', 'all_credentials', 'True')
     with open(config.path, 'w') as configfile:
         config.parser.write(configfile)
 
