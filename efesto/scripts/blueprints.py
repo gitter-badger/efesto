8c1bc9739bb06da393d889ce1c8aaaa858e8542d
Jacopo Cascioli <jacopocascioli@gmail.com>
2016-11-11 09:28:22 +0100

    feat(scripts): efesto-blueprints supports safe mode

--- efesto/scripts/blueprints.py
+++ efesto/scripts/blueprints.py
@@ -10,10 +10,10 @@ import os
 from efesto.Blueprints import dump_blueprint, load_blueprint
 
 
-def load(file_to_load):
+def load(file_to_load, mode):
     path = os.path.join(os.getcwd(), file_to_load)
     if os.path.isfile(path):
-        load_blueprint(path)
+        load_blueprint(path, mode=mode)
         print('Blueprint loaded successfully!')
     else:
         print('The provided path is not a file.')
@@ -29,9 +29,10 @@ def blueprints():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dump', dest='dump')
     parser.add_argument('--load', dest='load')
+    parser.add_argument('--load-mode', dest='load_mode', default='default')
     args = parser.parse_args()
     if args.load:
-        load(args.load)
+        load(args.load, args.load_mode)
 
     if args.dump:
         dump(args.dump)
